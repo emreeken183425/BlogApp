4c7e3045b20579d9348788ca4d582c9d0a7bbfd2
emreeken183425 <emreeken1834@gmail.com>
2022-09-09 22:50:30 +0300

    sign-up

---
+++ users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+
+def sign_up(request):
+    return render(request,'users/sign_up.html')
+
