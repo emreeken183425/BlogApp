19fd635011af1d5ab809352b5b3e640d15282f78
emreeken183425 <emreeken1834@gmail.com>
2022-09-09 22:32:01 +0300

    xz

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import PostModel
 
 class PostModelForm(forms.ModelForm):
-    
+    content=forms.CharField(widget=forms.Textarea(attrs={'row':4} ))
     class Meta:
         model = PostModel
         fields = ("title","content")
