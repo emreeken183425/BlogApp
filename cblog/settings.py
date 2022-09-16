c35c6cc229a192e8a713442ed997894a8fa27309
emreeken183425 <emreeken1834@gmail.com>
2022-09-16 16:34:15 +0300

    v

--- cblog/settings.py
+++ cblog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -128,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / "media_root"
 MEDIA_URL = '/media/'
 #STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
